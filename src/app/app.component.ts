5c3c214f91bc5e40320fe95bc4d9c7aea30460a4
Malik Sajid <engr.maliksajidkhan@gmail.com>
2019-11-10 15:03:59 +0500

    Angular Template Driven Form with Error Handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { EnrollmentService } from './enrollment.service';
+import { User } from './user';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'template-driven-form';
+  topics=['Angular', 'Vue', 'React'];
+  errorMessage='';
+
+  constructor(private __enrol:EnrollmentService){}
+
+  userModel=new User('Sajid Javed', 'engr.maliksajidkhan@gmail.com', '03099246697', 'Vue', 'Evening(5PM-8PM)', true);
+
+  onSubmit(){
+    this.__enrol.enrol(this.userModel)
+    .subscribe(
+      data => console.log("Success"+ data),
+      error=> this.errorMessage = error
+    )
+  }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,11 @@
+export class User {
+    constructor(
+        private name:string,
+        private email:string,
+        private phone:string,
+        private topic:string,
+        private timePreference: string,
+        private subscribe: boolean
+
+    ){}
+}
